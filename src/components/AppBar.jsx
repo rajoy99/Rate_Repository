e72c0e8fa28453edac7958e2cf68f15e2d3bf6e4
rajoy99 <rafat.joy99@gmail.com>
2022-02-18 01:39:46 +0600

    Cool features

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,16 +1,24 @@
-import { View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import { Pressable,Text, View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
+    backgroundColor:'#24292e',
+    flexGrow:1
     // ...
   },
   // ...
 });
 
 const AppBar = () => {
-  return <View style={styles.container}>{/* ... */}</View>;
+  return (
+    <View style={styles.container}>
+      <Pressable>
+        <Text style={{color:'white',fontSize:28}}>Repositories</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 export default AppBar;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,11 @@ import Constants from 'expo-constants';
 import { StyleSheet, View } from 'react-native';
 import RepositoryList from './RepositoryList';
 import Text from './Text';
+import AppBar from './AppBar';
 
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: Constants.statusBarHeight,
     flexGrow: 1,
     flexShrink: 1,
   },
@@ -41,8 +41,7 @@ const FlexboxExample = () => {
 const Main = () => {
   return (
     <View style={styles.container}>
-
-      <FlexboxExample />
+      <AppBar />
       <Text>Rate Repository Application</Text>
       <Text color="textSecondary">Text with secondary color</Text>
       <RepositoryList />
